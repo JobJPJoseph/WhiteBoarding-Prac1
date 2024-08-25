ca02f133a518a1d9b4ce67769252f378231c720e
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-08-25 00:11:27 -0400

    We passed the tests for isPrime

--- test/whiteboarding-problems1.spec.js
+++ test/whiteboarding-problems1.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { logBetween, logBetweenStepper, printReverse, fizzBuzz
+const { logBetween, logBetweenStepper, printReverse, fizzBuzz, isPrime
 } = require('../lib/whiteboarding-problems1');
 
 describe('logBetween' , function () {
@@ -35,6 +35,18 @@ describe('fizzBuzz', function () {
 
     it('should takes a number and returns an array of every number from 0 to max that is divisible by either 3 or 5, but not both.', function () {
         expect(fizzBuzz(20)).to.deep.equal([3, 5, 6, 9, 10, 12, 18]);
+    });
+
+});
+
+describe('isPrime', function () {
+
+    it('should returns true if number is prime. Otherwise, false. Assume number is a positive integer.', function () {
+        expect(isPrime(2)).to.be.true;
+        expect(isPrime(10)).to.be.false;
+        expect(isPrime(11)).to.be.true;
+        expect(isPrime(9)).to.be.false;
+        expect(isPrime(2017)).to.be.true;
     });
 
 });
